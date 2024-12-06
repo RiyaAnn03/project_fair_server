7b673a70f18cf0ae0d95516c87797578b70ec3be
RiyaAnn03 <riyaanng@gmail.com>
2024-12-06 12:27:30 +0530

    incomplete v2

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,22 @@
 const express=require('express')
 const userController=require('../controllers/userController')
+const projectController=require('../controllers/projectController')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
+const multerMiddleware = require('../middlewares/multerMiddleware')
 const router =new express.Router()
 
 // register-post
 router.post('/register',userController.registerController)
 // login-post
 router.post('/login',userController.loginController)
+// add-project
+router.post('/add-project',jwtMiddleware,multerMiddleware.single('projectImage'),projectController.addprojectController)
+// home project-get
+router.get('/home-projects',projectController.getHomeProjectsController)
+// user-project-get
+router.get('/user-projects',jwtMiddleware,projectController.getUserProjectsController)
+// all-project-get
+router.get('/all-projects',jwtMiddleware,projectController.getAllProjectsController)
+
 
 module.exports=router
